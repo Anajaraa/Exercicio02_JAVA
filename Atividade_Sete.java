3d48df53a7c5c4a6e6603d101576f6c71e2bde91
Anajara Lucas <90160367+Anajaraa@users.noreply.github.com>
2024-06-26 21:10:27 -0300

    Update Atividade_Sete.java
    melhorias com a ajuda do netinho

--- Atividade_Sete.java
+++ Atividade_Sete.java
@@ -1,18 +1,41 @@
 public class Atividade_Sete {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7};
-        int n = 3;
+   public static void main(String[] args) {
+     
+        int[] lista = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Array original: " + Arrays.toString(lista));
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Número de posições a serem rotacionadas (de 1 a 9): ");
+        int n = entrada.nextInt();
+
+        System.out.println("Digite 'd' para rotacionar para a direita ou 'e' para rotacionar para a esquerda: ");
+        char r = entrada.next().charAt(0);
+
         boolean esquerda = true;
 
-        int[] rotacionado = rotacionar(array, n, esquerda);
-
-        System.out.println("Array rotacionado:");
-        for (int num : rotacionado) {
-            System.out.print(num + " ");
+        switch (r) {
+            case 'd':
+                esquerda = false;
+                break;
+            case 'e':
+                esquerda = true;
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
         }
+
+        int[] rotacionado = rodar(lista, n, esquerda);
+
+        System.out.println("Array rotacionado: " + Arrays.toString(rotacionado));
+        
+        entrada.close();
+
     }
 
-    public static int[] rotacionar(int[] array, int n, boolean esquerda) {
+    public static int[] rodar(int[] array, int n, boolean esquerda) {
         int tamanho = array.length;
         int[] novoArray = new int[tamanho];
 
